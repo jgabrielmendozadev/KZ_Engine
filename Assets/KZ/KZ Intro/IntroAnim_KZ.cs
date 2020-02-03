908bcb42cb15442257372932f878fa5eef6c3aae
Gabriel <JorgeGMendoza@users.noreply.github.com>
2020-02-03 01:49:46 -0300

    Intro Scene (KZ intro) added

---
+++ Assets/KZ/KZ Intro/IntroAnim_KZ.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace KZ {
+
+    public class IntroAnim_KZ : UnityEngine.MonoBehaviour {
+
+        //Const
+        const string SCENE_NAME = "0.KZ";
+
+        //Static
+        static Action _OnAnimationEnd;
+        public static void Show(float fadeOutTime, float fadeInTime, Action OnAnimationEnd) {
+            _OnAnimationEnd = OnAnimationEnd;
+            SceneManager.LoadScene(SCENE_NAME, fadeOutTime, fadeInTime);
+        }
+
+        //Instance
+        void OnFinishAnimation() {
+            _OnAnimationEnd?.Invoke();
+            _OnAnimationEnd = null;
+        }
+
+
+    }
+
+}
---
+++ Assets/KZ/Scripts/Mgr/SceneManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using USM = UnityEngine.SceneManagement.SceneManager;
+
+namespace KZ {
+    public static class SceneManager {
+
+
+
+        public static void LoadSceneInstant(string sceneName) {
+            USM.LoadScene(sceneName);
+        }
+
+        public static bool LoadScene(string sceneName, float fadeOutTime = 0.4f, float fadeInTime = 1.0f) {
+            var at = AppTransition.instance;
+            if (at != null && !at.isTransitioning) {
+                at.AnimateTransition(fadeOutTime, fadeInTime, () => USM.LoadScene(sceneName), null);
+                return true;
+            }
+            return false;
+        }
+
+
+
+        const string
+            SPLASH = "0.LoadScene",
+            GAME = "1.IntroGame";
+
+        public static void LoadSplashScene() => LoadSceneInstant(SPLASH);
+        public static void LoadGameScene() => LoadScene(GAME);
+
+        public static void LoadIntroScene() {
+            IntroAnim_KZ.Show(0, 0.5f, LoadGameScene);
+        }
+    }
+}
